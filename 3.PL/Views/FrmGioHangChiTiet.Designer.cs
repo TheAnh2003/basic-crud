44a0239e831543e80ff9da7db307135df0b319c8
Carp Koi <xamhocvn@gmail.com>
2022-10-20 20:38:03 +0700

    test2

--- 3.PL/Views/FrmGioHangChiTiet.Designer.cs
+++ 3.PL/Views/FrmGioHangChiTiet.Designer.cs
@@ -29,21 +29,55 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(143, 134);
+            this.comboBox1.Location = new System.Drawing.Point(96, 122);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(151, 28);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "chttps://github.com/c4rpkoi/PTPM_FPOLY_SOF205_khoitmph23666.git";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(393, 122);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "chttps://github.com/c4rpkoi/PTPM_FPOLY_SOF205_khoitmph23666.git";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(259, 219);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "chttps://github.com/c4rpkoi/PTPM_FPOLY_SOF205_khoitmph23666.git";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(404, 271);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 28);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.Text = "chttps://github.com/c4rpkoi/PTPM_FPOLY_SOF205_khoitmph23666.git";
             // 
             // FrmGioHangChiTiet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Name = "FrmGioHangChiTiet";
             this.Text = "FrmGioHangChiTiet";
@@ -54,5 +88,8 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
     }
 }
