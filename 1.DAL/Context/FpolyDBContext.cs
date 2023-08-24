46d84d1b33d1e1bc0a7ec08837c55896d36f0a5a
TheAnh2003 <thean13321@gmail.com>
2023-08-24 14:47:42 +0700

    1

--- 1.DAL/Context/FpolyDBContext.cs
+++ 1.DAL/Context/FpolyDBContext.cs
@@ -37,7 +37,7 @@ namespace _1.DAL.Context
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=CARPKOI\\SQLEXPRESS01;Initial Catalog=FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041;Persist Security Info=True;User ID=khoitmph23666;Password=123456");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-L9J8TJS;Initial Catalog=No1112;Integrated Security=True");
             }
         }
 
